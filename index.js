a54c8b991af84eb951d31610f0702147a4558233
unknown <coder.rakibul@gmail.com>
2022-05-24 20:15:03 +0600

    admin api updated

--- index.js
+++ index.js
@@ -50,14 +50,11 @@ async function run() {
 
         app.put('/user/admin/:email', async (req, res) => {
             const email = req.params.email;
-
             const filter = { email: email };
-
             const updateDoc = {
                 $set: { role: 'admin' },
             };
             const result = await userCollection.updateOne(filter, updateDoc);
-
             res.send(result);
         });
 
